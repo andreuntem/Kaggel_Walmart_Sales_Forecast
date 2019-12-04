ef479131bbcf3f93b6ddad620522d9c949371009
andreuntem <andre.untem@gmail.com>
2019-12-04 23:34:25 +0000

    init

--- WM_sales_fc.py
+++ WM_sales_fc.py
@@ -2,6 +2,12 @@
 KAGGLE - WALMAR RECRUITING - SALES FORECAST
 https://www.kaggle.com/c/walmart-recruiting-store-sales-forecasting
 
+Questions:
+    1. What are the biggest stores?
+    2. What are the biggest departments?
+    3. Stores and Dept are seasonal?
+    4. What is the holiday impact?
+    5. 
 """
 
 # ========================================================================
@@ -15,10 +21,10 @@ import pandas as pd
 # IMPORT DATA
 # ========================================================================
 df = pd.read_csv('train.csv')
-
+df['Date_str']=df['Date'].apply(lambda x: str(x))
 
 stores = pd.read_csv('stores.csv')
-print(stores.head(10))
+
 
 
 

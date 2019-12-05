78d8162b213b34463d6356a83c8cb601405fdea0
andreuntem <andre.untem@gmail.com>
2019-12-05 00:20:23 +0000

    init

--- WM_sales_fc.py
+++ WM_sales_fc.py
@@ -3,18 +3,28 @@ KAGGLE - WALMAR RECRUITING - SALES FORECAST
 https://www.kaggle.com/c/walmart-recruiting-store-sales-forecasting
 
 Questions:
-    1. What are the biggest stores?
-    2. What are the biggest departments?
-    3. Stores and Dept are seasonal?
-    4. What is the holiday impact?
-    5. 
+    1. What are the biggest stores? (45)
+    2. What are the biggest departments? (98)
+    3. How many departments by store?
+    4. How is it related with type of store? size or departments in store?
+    5. Stores and Dept are seasonal?
+    6. What is the holiday impact?
+
+Objective:
+    Minimize WMAE (weighted mean absolute error),
+    weight = 5 if holiday, else 1
 """
+
 
 # ========================================================================
 # IMPORT LIBRARIES
 # ========================================================================
 import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 
 # ========================================================================
@@ -25,7 +35,11 @@ df['Date_str']=df['Date'].apply(lambda x: str(x))
 
 stores = pd.read_csv('stores.csv')
 
+sns.jointplot('Type','Size',data=stores)
+plt.show()
 
+pvt = pd.pivot_table(df,index='Store',values='Weekly_Sales')
+pvt = pvt.reset_index()
 
 
 
